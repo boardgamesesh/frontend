3c0bad93946edf97229b7d8e2d5d69e0e36fe157
Geoffrey Chong <gyf.chong@gmail.com>
2022-11-15 18:00:07 +1100

    fix: deps and added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@styles/globals.css";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
