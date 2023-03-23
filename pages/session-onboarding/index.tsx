7c4ec9139f507642ce96185d8ea4fbee9c266224
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-03-23 13:43:45 +0800

    updated according to comments

--- pages/session-onboarding/index.tsx
+++ pages/session-onboarding/index.tsx
@@ -1,44 +1,99 @@
 // import Link from "next/link";
 import React, { HTMLAttributes } from "react";
 // import { Heading, Button, Input, Text, Label } from "@honeycomb";
-
 import styles from "./session-onboarding.module.css";
 
 export default function Page() {
   return (
     <>
       <div className={styles.container}>
-        <h3 className={styles.title}>Who&apos;s invited?</h3>
-        {/* Text/Header component*/}
-        <div className={styles.invitesContainer}>
-          <input
-            type="text"
-            placeholder="Enter friend's email"
-            className={styles.invites}
-          />
-          {/* Could be an Input component since the app will contain more than one*/}
-          <button className={styles.add} type="submit">
-            Add
-          </button>
-          {/* Text component maybe  */}
-        </div>
-        <div className={styles.addedEmails}>
-          {/* Text component maybe */}
-          <p className={styles.emailAdded}>
-            Tywin@lanister.com <span className={styles.deleteEmail}> &#10005; </span> {/* Text component*/}
-          </p>
-          {/* Text component maybe */}
-          <p className={styles.emailAdded}>
-            madKing@lanister.com <span className={styles.deleteEmail}> &#10005; </span>
-          </p>
-        </div>
+        <form>
+          <h1 className={styles.title}>Who&apos;s invited?</h1>
+          {/* Text/Header component*/}
+          <div className={styles.invitesContainer}>
+            <label htmlFor="email" className={styles.emailLabel}>
+              Enter your friend&apos;s email address bellow
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className={styles.emailInput}
+              />
+            </label>
 
-        <div className={styles.actionButtons}>
-          {/* Text component maybe */}
-          <button className={styles.inviteButton}>Invite friends</button>
-          <button className={styles.copyButton}> Copy invite link </button>
-          <button className={styles.skipButton}>Skip this step</button>
-        </div>
+            {/* Could be an Input component since the app will contain more than one*/}
+            <button className={styles.add} type="button">
+              Add
+            </button>
+            {/* Text component maybe  */}
+          </div>
+          <div className={styles.addedEmails}>
+            {/* Text component maybe */}
+
+            {/* These would be added dinamicaly. A scroll bar for this section would come in handy for cases where the list of emails ends up being rather long*/}
+            <div className={styles.emailAdded}>
+              Tywin@lanister.com
+              <button
+                className={styles.deleteEmail}
+                aria-label="remove"
+                type="button"
+              >
+                &times;
+              </button>
+            </div>
+            <div className={styles.emailAdded}>
+              Tywin@lanister.com
+              <button
+                className={styles.deleteEmail}
+                aria-label="remove"
+                type="button"
+              >
+                &times;
+              </button>
+            </div>
+            <div className={styles.emailAdded}>
+              averylongemail.address_here@email.com
+              <button
+                className={styles.deleteEmail}
+                aria-label="remove"
+                type="button"
+              >
+                &times;
+              </button>
+            </div>
+            <div className={styles.emailAdded}>
+              Tywin@lanister.com
+              <button
+                className={styles.deleteEmail}
+                aria-label="remove"
+                type="button"
+              >
+                &times;
+              </button>
+            </div>
+
+            {/* Text component maybe */}
+            <div className={styles.emailAdded}>
+              madKing@lanister.com
+              <button className={styles.deleteEmail} aria-label="remove">
+                &times;
+              </button>
+            </div>
+          </div>
+
+          <div className={styles.actionButtons}>
+            {/* Text component maybe */}
+            <button className={styles.inviteButton} type="button">
+              Invite friends
+            </button>
+            <button className={styles.copyButton} type="button">
+              Copy invite link
+            </button>
+            <button className={styles.skipButton} type="button">
+              Skip this step
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
