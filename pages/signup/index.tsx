74097b88b03ff5407f2600b2bbd71bb45171ccbc
Geoffrey Chong <gyf.chong@gmail.com>
2023-04-02 12:35:21 +1000

    fix apollo rename

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { gql, useMutation, useQuery } from "~apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 
 import { useEffect } from "react";
 
