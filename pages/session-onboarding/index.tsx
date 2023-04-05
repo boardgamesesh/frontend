670ecde2952ac8d7d76c7677dbd23e9373954475
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-04-05 12:45:35 +0800

    functionality for adding emails dynamically to DOM

--- pages/session-onboarding/index.tsx
+++ pages/session-onboarding/index.tsx
@@ -1,68 +1,81 @@
-// import Link from "next/link";
-import React, { HTMLAttributes } from "react";
-// import { Heading, Button, Input, Text, Label } from "../../packages/honeycomb";
+import React, { HTMLAttributes, useState } from "react";
 import styles from "./session-onboarding.module.css";
 import Link from "next/link";
+import clsx from "clsx";
 
 export default function Page() {
+  const [email, setEmail] = useState<string>("");
+  const [emails, setEmails] = useState<string[]>([]);
+  const [focus, setFocus] = useState<any>(false);
+
   return (
     <>
       <div className={styles.container}>
         <form>
           <h1 className={styles.title}>Who&apos;s invited?</h1>
+
           {/* Text/Header component*/}
           <div className={styles.invitesContainer}>
             <label htmlFor="email" className={styles.emailLabel}>
               Enter your friend&apos;s email address bellow
             </label>
-            <div className={styles.enterEmail}>
+            <div
+              className={clsx(styles.enterEmail, {
+                [styles.darkborder]: focus,
+              })}
+            >
+              {/* Could be an Input component since the app will contain more than one*/}
               <input
                 type="email"
                 id="email"
                 name="email"
+                placeholder="mary.poppins@email.com"
                 className={styles.emailInput}
+                onChange={(e) => {
+                  setEmail(e.currentTarget.value);
+                }}
+                value={email}
+                onFocus={() => setFocus(true)}
+                onBlur={() => setFocus(false)}
               />
-              {/* Could be an Input component since the app will contain more than one*/}
-              <button className={styles.addButton} type="button">
+              <button
+                id="add"
+                className={styles.addButton}
+                type="button"
+                onClick={() => { 
+                  // Validation can be improved or replaced with a library..
+                  let regExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+                  
+                  if (!email || !regExp.test(email)) {
+                    alert("Please enter a valid email");
+                  } else {
+                    setEmail("");
+                    setEmails([...emails, email]);
+                  }
+                }}
+              >
                 Add
               </button>
             </div>
 
-            {/* Text component maybe  */}
           </div>
-
           <div className={styles.addedEmails}>
             {/* Text component maybe */}
-
-            {/* These would be added dinamicaly. A scroll bar for this section would come in handy for cases where the list of emails ends up being rather long*/}
-            <div className={styles.emailAdded}>
-              Tywin@lanister.com
-              <button
-                className={styles.deleteEmail}
-                aria-label="remove"
-                type="button"
-              >
-                &times;
-              </button>
-            </div>
-            <div className={styles.emailAdded}>
-              averylongemail.address_here@email.com
-              <button
-                className={styles.deleteEmail}
-                aria-label="remove"
-                type="button"
-              >
-                &times;
-              </button>
-            </div>
-
-            {/* Text component maybe */}
-            <div className={styles.emailAdded}>
-              madKing@lanister.com
-              <button className={styles.deleteEmail} aria-label="remove">
-                &times;
-              </button>
-            </div>
+            {emails.map((email, index) => (
+              <div className={styles.emailAdded} key={index}>
+                {email}
+                <button
+                  className={styles.deleteEmail}
+                  aria-label="remove"
+                  type="button"
+                  onClick={() => {
+                    setEmails(emails.filter((em) => em !== email));
+                  }}
+                >
+                  &times;
+                </button>
+              </div>
+            ))}
           </div>
 
           <div className={styles.actionButtons}>
