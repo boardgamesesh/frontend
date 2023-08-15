6315a490f2352bfb7dceeb6561d2bf4ffcab96e4
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-08-15 09:41:38 +0800

    Added type prop to btn component

--- packages/honeycomb/Button/Button.tsx
+++ packages/honeycomb/Button/Button.tsx
@@ -8,12 +8,14 @@ export const Button = ({
   size,
   mood,
   onClick,
+  type
 }: {
   children: ReactNode;
   onClick: () => void;
   intent?: string;
   size?: string;
   mood?: string;
+  type?: "submit" | "button" | "reset"
 }) => (
   <button
     onClick={onClick}
@@ -23,6 +25,7 @@ export const Button = ({
       [styles.small]: size === "small",
       [styles.large]: size === "large",
     })}
+    type={type}
   >
     {children}
   </button>
