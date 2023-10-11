0acafb439c2dd1b79f9c520e0b82a22a328e81db
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-10-11 16:15:53 +0800

    simplified UI for MUI inputs

--- app/(workflows)/new-session/time-and-place/page.tsx
+++ app/(workflows)/new-session/time-and-place/page.tsx
@@ -4,12 +4,15 @@ import React, { startTransition, useState } from "react";
 import styles from "./time-and-place.module.css";
 import { Button } from "~honeycomb";
 
-import { DatePicker, TimePicker, TimeClock } from "@mui/x-date-pickers";
+import { DatePicker, TimePicker } from "@mui/x-date-pickers";
 
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { format } from "date-fns";
 
 export default function Page() {
+  const [value, setValue] = useState<Date | string | null>(new Date());
+  
   return (
     <>
       <div className={styles.container}>
@@ -18,33 +21,29 @@ export default function Page() {
           <form className={styles.timeAndDateForm}>
             <fieldset className={styles.fieldset}>
               <div className={styles.gameDate}>
-                <label className={styles.label}>Date</label>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DatePicker
-                    className={styles.inputDT}
-                    label="Select a date"
-                    slotProps={{
-                      textField: {
-                        helperText: "MM/DD/YYYY",
-                      },
-                    }}
-                  />
-                </LocalizationProvider>
+                <label className={styles.label}>
+                  Date
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DatePicker
+                      className={styles.inputDT}
+                      value={value}
+                      onChange={(newValue) => setValue(newValue)}
+                    />
+                  </LocalizationProvider>
+                </label>
               </div>
 
               <div className={styles.gameTime}>
-                <label className={styles.label}>Time</label>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <TimePicker
-                    className={styles.inputDT}
-                    label="Select a time"
-                    slotProps={{
-                      textField: {
-                        helperText: "HH:MM AM/PM",
-                      },
-                    }}
-                  />
-                </LocalizationProvider>
+                <label className={styles.label}>
+                  Time
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <TimePicker
+                      className={styles.inputDT}
+                      value={value}
+                      onChange={(newValue) => setValue(newValue)}
+                    />
+                  </LocalizationProvider>
+                </label>
               </div>
             </fieldset>
             <fieldset className={styles.fieldset}>
@@ -63,8 +62,10 @@ export default function Page() {
               </div>
             </fieldset>
 
-            <Button onClick={() => { }} type="submit"> Submit </Button>
-
+            <Button onClick={() => {}} type="submit">
+              {" "}
+              Submit{" "}
+            </Button>
           </form>
         </div>
       </div>
