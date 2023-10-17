8a622125679de3a8f4763be80c542c2d5c8824fa
InOmniaParatus-WD <78725314+InOmniaParatus-WD@users.noreply.github.com>
2023-10-17 11:57:31 +0800

    update and simplified

--- packages/honeycomb/TextField/TextField.tsx
+++ packages/honeycomb/TextField/TextField.tsx
@@ -1,4 +1,5 @@
 import React, { InputHTMLAttributes, LabelHTMLAttributes } from "react";
+import { useId } from "react";
 import clsx from "clsx";
 import styles from "./TextField.module.css";
 
@@ -6,34 +7,25 @@ interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   type?: "text" | "email";
   name?: string;
   label: string;
-  title?: string;
-  field?: string;
   onChange: () => void;
 }
 
-export const TextField = ({
-  name,
-  type,
-  label,
-  title,
-  field,
-  ...props
-}: TextFieldProps) => (
-  <fieldset className={clsx(styles.base)}>
-    <label
-      htmlFor={name}
-      className={clsx(styles.base, { [styles.locationLabel]: title === "locationLabel" })}
-    >
-      {label}
-    </label>
-    <input
-      className={clsx(styles.base, {
-        [styles.locationInput]: field === "locationInput",
-      })}
-      type={type}
-      id={name}
-      name={name}
-      {...props}
-    />
-  </fieldset>
-);
+export const TextField = ({ name, type, label, ...props }: TextFieldProps) => {
+  const id = useId();
+
+  return (
+    <div className={clsx(styles.base)}>
+      <label htmlFor={id} className={clsx(styles.label)}>
+        {label}
+      </label>
+      <input
+        className={clsx(styles.input)}
+        type={type}
+        aria-describedby={id}
+        id={id}
+        name={name}
+        {...props}
+      />
+    </div>
+  );
+};
