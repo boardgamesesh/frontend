f9261b6668e14022122d4405ad68ebbe2b55ea7e
Geoffrey Chong <gyf.chong@gmail.com>
2023-10-21 14:55:42 +1100

    replace jest axe with vitest axe

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,10 +1,5 @@
 import { getStoryContext, TestHook } from "@storybook/test-runner";
 import { configureAxe, getAxeResults, injectAxe } from "axe-playwright";
-import { toHaveNoViolations } from "jest-axe";
-
-export const setup: () => void = () => {
-  expect.extend(toHaveNoViolations);
-};
 
 export const preRender: TestHook = async (page) => {
   await injectAxe(page);
