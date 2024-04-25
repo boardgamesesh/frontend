037b231c65a5f0eeca9971a9b43da243d0d0083c
Geoffrey Chong <gyf.chong@gmail.com>
2024-04-25 15:42:28 +1000

    update metadata

--- apps/session/app/layout.tsx
+++ apps/session/app/layout.tsx
@@ -6,7 +6,22 @@ import { cn } from "../lib/utils";
 
 export const metadata: Metadata = {
   title: "BoardGameSesh",
-  description: "Helping you organise your next boardgame group",
+  description: "Organise your next boardgame session with ease.",
+  openGraph: {
+    title: "BoardgameSesh",
+    description: "Organise your next boardgame session with ease.",
+    url: "https://boardgamesesh.com",
+    siteName: "BoardgameSesh",
+    images: [
+      {
+        url: "../public/logomark.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en-AU",
+    type: "website",
+  },
 };
 
 const fontSans = FontSans({
