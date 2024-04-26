54c72340457dc11c48aa77ad5fd14276d958094e
Geoffrey Chong <gyf.chong@gmail.com>
2024-04-26 14:34:08 +1000

    simplify ui

--- apps/session/app/page.tsx
+++ apps/session/app/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import {
   Form,
   FormControl,
@@ -9,15 +16,17 @@ import {
   FormItem,
   FormLabel,
 } from "@/components/ui/form";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { Input } from "@/components/ui/input";
 import {
+  AlertOctagon,
+  BadgeInfo,
   ChevronRight,
   DicesIcon,
   Info,
   Loader2,
   Plus,
   Sparkles,
+  Terminal,
   X,
 } from "lucide-react";
 import { useEffect, useState } from "react";
@@ -29,12 +38,28 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
 import Logo from "@/public/logo.svg";
+import { Toggle } from "@/components/ui/toggle";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Home() {
   const [players, setPlayers] = useState<string[]>([]);
   const [picker, setPicker] = useState<string | null>();
-  const [seshNo, setSeshNo] = useState<number>(1);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [config, setConfig] = useState({ keepPlayers: false });
 
   // Get data from local storage
   useEffect(() => {
@@ -49,12 +74,6 @@ export default function Home() {
 
       if (picker) {
         setPicker(picker);
-      }
-
-      if (seshNo) {
-        setSeshNo(parseInt(seshNo, 10));
-      } else {
-        localStorage.setItem("seshNo", "1");
       }
     }
   }, []);
@@ -127,145 +146,219 @@ export default function Home() {
   };
 
   const handleNewSesh = () => {
-    const newSeshNo = seshNo + 1;
-
-    setSeshNo(newSeshNo);
-    localStorage.setItem("seshNo", newSeshNo.toString());
     setPicker(null);
     localStorage.removeItem("picker");
+
+    if (!config.keepPlayers) {
+      setPlayers([]);
+      localStorage.removeItem("players");
+    }
   };
 
   return (
     <main>
-      <div className="container max-w-lg space-y-6">
-        <header>
-          <h1 className="font-bold mt-6 text-2xl flex gap-2 items-center">
+      <div className="container max-w-lg py-6 space-y-6">
+        <header className="flex items-center justify-between">
+          <h1 className="font-bold text-xl flex gap-2 items-center">
             <Image src={Logo} alt="" className="h-6 w-6" />
-            BoardGameSesh
+            BGSesh
           </h1>
-        </header>
-        <div className="flex-grow">
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between gap-4 space-y-0 border-b mb-6">
-              <CardTitle>Sesh #{seshNo}</CardTitle>
-              <Button
-                onClick={() => handleNewSesh()}
-                variant="outline"
-                className="gap-1"
-              >
-                Next sesh <ChevronRight className="h-4 w-4" />
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="outline" tabIndex={1}>
+                Start new sesh
               </Button>
-            </CardHeader>
-            <CardContent>
-              <div className="pb-6">
-                <div className="space-y-3">
-                  <div className="space-y-1">
-                    <h3 className="text-lg font-semibold">
-                      Players ({players.length})
-                    </h3>
-                    <p className="text-sm">
-                      Add at least 2 players to this session.
-                    </p>
-                  </div>
-                  <Form {...form}>
-                    <form
-                      onSubmit={form.handleSubmit(onSubmit)}
-                      className="w-full"
-                    >
-                      <FormField
-                        control={form.control}
-                        name="playerName"
-                        render={({ field }) => (
-                          <FormItem className="relative space-y-0">
-                            <FormLabel className="absolute border-r p-2 mt-[5px] font-bold">
-                              Name
-                            </FormLabel>
-                            <div className="flex gap-4">
-                              <FormControl>
-                                <Input
-                                  type="text"
-                                  className="pl-[4rem]"
-                                  minLength={1}
-                                  required
-                                  {...field}
-                                />
-                              </FormControl>
-                              <Button type="submit" className="gap-2 h-[40px]">
-                                <Plus className="h-4 w-4" /> Add{" "}
-                                <span className="sr-only">
-                                  {field.value} to players
-                                </span>
-                              </Button>
-                            </div>
-                          </FormItem>
-                        )}
-                      />
-                    </form>
-                  </Form>
-                  {players.length > 0 && (
-                    <>
-                      <Separator className="my-3" />
-                      <ul className="grid gap-4">
-                        {players.map((player) => (
-                          <li
-                            key={player}
-                            className="flex justify-between items-center"
-                          >
-                            {player}
-                            <Button
-                              type="button"
-                              size="icon"
-                              variant="secondary"
-                              className="text-xs h-8 w-8"
-                              onClick={() => handleRemovePlayer(player)}
-                            >
-                              <X className="h-4 w-4" />
-                              <span className="sr-only">Remove {player}</span>
-                            </Button>
-                          </li>
-                        ))}
-                      </ul>
-                    </>
-                  )}
-                </div>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This will delete your current sesh and start a new one.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <div className="border rounded-lg p-4 flex justify-between items-center">
+                <Label className=" grid gap-1" htmlFor="keepPlayers">
+                  <span className="text-sm font-semibold">
+                    Transfer existing players
+                  </span>
+                  <span className="text-xs text-gray-500">
+                    Keep your current players for the next sesh.
+                  </span>
+                </Label>
+                <Switch
+                  id="keepPlayers"
+                  checked={config.keepPlayers}
+                  onCheckedChange={(event) => {
+                    setConfig((prevState) => ({
+                      ...prevState,
+                      keepPlayers: event,
+                    }));
+                  }}
+                />
               </div>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={() => handleNewSesh()}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </header>
+        <Card className="relative">
+          <CardHeader>
+            <CardTitle>Players ({players.length})</CardTitle>
+            <CardDescription>
+              Add at least 2 players to this session.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-3">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+                <FormField
+                  control={form.control}
+                  name="playerName"
+                  render={({ field }) => (
+                    <FormItem className="relative space-y-0">
+                      <FormLabel className="absolute border-r p-2 mt-[5px] font-bold">
+                        Name
+                      </FormLabel>
+                      <div className="flex gap-4">
+                        <FormControl>
+                          <Input
+                            type="text"
+                            className="pl-[4rem]"
+                            minLength={1}
+                            required
+                            tabIndex={1}
+                            {...field}
+                          />
+                        </FormControl>
+                        <Button
+                          type="submit"
+                          className="gap-2 h-[40px]"
+                          tabIndex={2}
+                        >
+                          <Plus className="h-4 w-4" />
+                          <span className="sr-only md:not-sr-only">Add</span>
+                          <span className="sr-only">
+                            {field.value} to players
+                          </span>
+                        </Button>
+                      </div>
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
 
-              {players.length >= 2 && (
-                <div aria-live="polite">
-                  <Separator className="mb-6" />
-                  <div className="space-y-1 my-4">
-                    <h2 className="text-lg font-semibold">Game picker</h2>
-                    <p className="text-sm">
-                      Determine who gets to choose the game for the sesh!
-                    </p>
-                  </div>
+            {players.length > 0 && (
+              <div className="mt-3">
+                <Separator className="my-3" />
+                <ul className="grid gap-4">
+                  {players.map((player) => (
+                    <li
+                      key={player}
+                      className="flex justify-between items-center"
+                    >
+                      <span aria-atomic>
+                        {player}
+                        <span className="sr-only">Added</span>
+                      </span>
+                      <Button
+                        type="button"
+                        size="icon"
+                        variant="secondary"
+                        className="text-xs h-8 w-8"
+                        onClick={() => handleRemovePlayer(player)}
+                        tabIndex={4}
+                      >
+                        <X className="h-4 w-4" />
+                        <span className="sr-only" aria-atomic>
+                          Remove {player}
+                        </span>
+                      </Button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </CardContent>
+          <div aria-live="polite" aria-relevant="additions text">
+            {players.length >= 2 && (
+              <CardFooter className="sticky bottom-0 py-3 border-t bg-white rounded-b flex-col space-y-3">
+                {picker && !isLoading && (
+                  <p>
+                    <strong>{picker}</strong> will pick the game!
+                  </p>
+                )}
+                <div className="relative">
                   <Button
                     size="sm"
-                    className="flex gap-1"
+                    className="flex gap-1 w-full"
                     onClick={() => randomisePicker()}
                     disabled={isLoading}
                     variant="outline"
+                    tabIndex={3}
                   >
                     {isLoading ? (
                       <>
-                        <Loader2 className="animate-spin" /> Randomising...
+                        <Loader2 className="animate-spin" />
                       </>
                     ) : (
                       <>
-                        <DicesIcon /> Randomise game picker
+                        <DicesIcon />
                       </>
                     )}
+                    Randomise game picker
                   </Button>
-                  {picker && !isLoading && (
-                    <p className="mt-4">
-                      <strong>{picker}</strong> will pick the game!
-                    </p>
+                  {!picker && (
+                    <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
+                      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-75"></span>
+                      <span className="relative inline-flex rounded-full h-3 w-3 bg-purple-500"></span>
+                    </span>
                   )}
                 </div>
-              )}
+              </CardFooter>
+            )}
+          </div>
+        </Card>
+        {/* {players.length >= 2 && (
+          <Card>
+            <CardHeader>
+              <CardTitle id="game-picker">Who's picking the game?</CardTitle>
+              <CardDescription>
+                Determine who gets to choose the game for the sesh!
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Button
+                size="sm"
+                className="flex gap-1"
+                onClick={() => randomisePicker()}
+                disabled={isLoading}
+                variant="outline"
+              >
+                {isLoading ? (
+                  <>
+                    <Loader2 className="animate-spin" /> Randomising...
+                  </>
+                ) : (
+                  <>
+                    <DicesIcon /> Randomise game picker
+                  </>
+                )}
+              </Button>
+              <div aria-live="assertive" aria-relevant="text additions">
+                {picker && !isLoading && (
+                  <p className="mt-4">
+                    <strong>{picker}</strong> will pick the game!
+                  </p>
+                )}
+              </div>
             </CardContent>
           </Card>
-        </div>
+        )} */}
       </div>
     </main>
   );
